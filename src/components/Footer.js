3b2289434ac37799b71be141e3617c01fb8508f6
Oleg <132208831+olegreimers@users.noreply.github.com>
2023-08-28 16:55:33 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,21 +12,4 @@ const FooterWrapper = styled.footer`
   margin: 20px 0;
 `;
 
-
-const FooterLink = styled(Link)`
-  margin: 15px;
-  display: block;
-  font-size: 16px;
-  transition: all 0.3s ease;
-  text-decoration: underline;
-  text-decoration-color: transparent;
-  color: ${(props) =>
-    props.faded ? props.theme.colorText(0.5) : props.theme.colorText(0.8)};
-
-  &:hover {
-    text-decoration-color: ${(props) =>
-      props.faded ? props.theme.colorText(0.4) : props.theme.colorText(0.6)};
-  }
-`;
-
 export default Footer;
